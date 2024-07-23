2d7b69a0fa8f4d47c373e60bf00ba0df50fdc208
kahnu044 <kahnucharan044@gmail.com>
2024-07-23 11:37:48 +0530

    validation updated to the send message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -7,12 +7,29 @@ const sendMessage = (req, res) => {
 
         const { to, from, body } = req.body;
 
+        const phoneNumberPattern = /^\+\d{1,15}$/;
+
+        if (!to || !phoneNumberPattern.test(to)) {
+            return res.status(400).json({ success: 'error', message: 'Invalid to phone number' });
+        }
+
+        if (!from || !phoneNumberPattern.test(from)) {
+            return res.status(400).json({ success: false, message: 'Invalid from phone number' });
+        }
+
+        if (!body || typeof body !== 'string' || body.trim() === '') {
+            return res.status(400).json({ success: false, message: 'Message body is required' });
+        }
+
         twilioClient.messages
             .create({ to, from, body })
             .then(message => {
                 res.json({
                     success: true,
-                    message: `Message sent with SID ${message.sid}`
+                    message: `Successfully message sent to :  ${message?.to}`,
+                    messageSid: message?.sid,
+                    createdAt: message?.dateCreated,
+                    updatedAt: message?.dateUpdated
                 });
             })
             .catch(error => {
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
 const PORT = 3003;
+
+const app = express();
+app.use(express.json());
+
 const messageRoutes = require('./routes/messageRoutes');
 
 // Welcome route
